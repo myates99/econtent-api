e19092c1578c4bfb41d08917389d3bd0a9542628
myates99 <zeac101@live.rhul.ac.uk>
2020-06-24 19:23:40 +0100

    Added functionality to the article CRUD methods.

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,38 +1,83 @@
+const Article = require('../models/Article');
+
 // @desc    Get all articles
 // @route   GET /api/v1/articles
 // @access  Public
-exports.getArticles = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all articles' });
+exports.getArticles = async (req, res, next) => {
+  try {
+    const articles = await Article.find();
+
+    res
+      .status(200)
+      .json({ success: true, count: articles.length, data: articles });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Get single article
 // @route   GET /api/v1/articles/:id
 // @access  Public
-exports.getArticle = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Get article ${req.params.id}` });
+exports.getArticle = async (req, res, next) => {
+  try {
+    const article = await Article.findById(req.params.id);
+
+    if (!article) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: article });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Create article
 // @route   POST /api/v1/articles
 // @access  Private
-exports.createtArticles = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Create new article' });
+exports.createtArticles = async (req, res, next) => {
+  try {
+    const article = await Article.create(req.body);
+
+    res.status(201).json({ success: true, data: article });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Update article
 // @route   PUT /api/v1/articles/:id
 // @access  Private
-exports.updateArticle = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update article ${req.params.id}` });
+exports.updateArticle = async (req, res, next) => {
+  try {
+    const article = await Article.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!article) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: article });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Delete article
 // @route   DELETE /api/v1/articles/:id
 // @access  Private
-exports.deleteArticle = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete article ${req.params.id}` });
+exports.deleteArticle = async (req, res, next) => {
+  try {
+    const article = await Article.findByIdAndDelete(req.params.id);
+
+    if (!article) {
+      return res.status(400).json({ success: false, data: {} });
+    }
+
+    res.status(200).json({ success: true });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
