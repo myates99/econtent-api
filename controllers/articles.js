1dc05f8ab0774d60a61d3ebe0ffa0d9106005d6e
myates99 <zeac101@live.rhul.ac.uk>
2020-06-24 20:24:49 +0100

    Added middleware to handle promise rejections.

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,90 +1,71 @@
 const Article = require('../models/Article');
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 
 // @desc    Get all articles
 // @route   GET /api/v1/articles
 // @access  Public
-exports.getArticles = async (req, res, next) => {
-  try {
-    const articles = await Article.find();
+exports.getArticles = asyncHandler(async (req, res, next) => {
+  const articles = await Article.find();
 
-    res
-      .status(200)
-      .json({ success: true, count: articles.length, data: articles });
-  } catch (err) {
-    next(err);
-  }
-};
+  res
+    .status(200)
+    .json({ success: true, count: articles.length, data: articles });
+});
 
 // @desc    Get single article
 // @route   GET /api/v1/articles/:id
 // @access  Public
-exports.getArticle = async (req, res, next) => {
-  try {
-    const article = await Article.findById(req.params.id);
+exports.getArticle = asyncHandler(async (req, res, next) => {
+  const article = await Article.findById(req.params.id);
 
-    if (!article) {
-      return next(
-        new ErrorResponse(`Article not found with id of ${err.value}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: article });
-  } catch (err) {
-    next(err);
+  if (!article) {
+    return next(
+      new ErrorResponse(`Article not found with id of ${err.value}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: article });
+});
 
 // @desc    Create article
 // @route   POST /api/v1/articles
 // @access  Private
-exports.createtArticles = async (req, res, next) => {
-  try {
-    const article = await Article.create(req.body);
+exports.createtArticles = asyncHandler(async (req, res, next) => {
+  const article = await Article.create(req.body);
 
-    res.status(201).json({ success: true, data: article });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(201).json({ success: true, data: article });
+});
 
 // @desc    Update article
 // @route   PUT /api/v1/articles/:id
 // @access  Private
-exports.updateArticle = async (req, res, next) => {
-  try {
-    const article = await Article.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+exports.updateArticle = asyncHandler(async (req, res, next) => {
+  const article = await Article.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!article) {
-      return next(
-        new ErrorResponse(`Article not found with id of ${err.value}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: article });
-  } catch (err) {
-    next(err);
+  if (!article) {
+    return next(
+      new ErrorResponse(`Article not found with id of ${err.value}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: article });
+});
 
 // @desc    Delete article
 // @route   DELETE /api/v1/articles/:id
 // @access  Private
-exports.deleteArticle = async (req, res, next) => {
-  try {
-    const article = await Article.findByIdAndDelete(req.params.id);
+exports.deleteArticle = asyncHandler(async (req, res, next) => {
+  const article = await Article.findByIdAndDelete(req.params.id);
 
-    if (!article) {
-      return next(
-        new ErrorResponse(`Article not found with id of ${err.value}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true });
-  } catch (err) {
-    next(err);
+  if (!article) {
+    return next(
+      new ErrorResponse(`Article not found with id of ${err.value}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true });
+});
