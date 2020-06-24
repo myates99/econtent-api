245c13d1c8abc56dff5a9d0573ac89e6d8f701b4
myates99 <zeac101@live.rhul.ac.uk>
2020-06-24 15:55:35 +0100

    Added routes for articles.

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/articles', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all articles' });
+});
+app.get('/api/v1/articles/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Get article ${req.params.id}` });
+});
+app.post('/api/v1/articles', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new article' });
+});
+app.put('/api/v1/articles/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update article ${req.params.id}` });
+});
+app.delete('/api/v1/articles/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete article ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
