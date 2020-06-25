dc8ecf8cfaca79d717dadc0ad5dffeaaee4e8103
myates99 <zeac101@live.rhul.ac.uk>
2020-06-25 13:40:00 +0100

    Implemented database seeder for Articles

---
+++ seeder.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Load models
+const Article = require('./models/Article');
+
+// Connect to BD
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+// Read JSON files
+const articles = JSON.parse(
+  fs.readFileSync(`${__dirname}/assets/_data/articles.json`, 'utf-8')
+);
+
+// Import into DB
+const importData = async () => {
+  try {
+    await Article.create(articles);
+
+    console.log('Data Imported...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Article.deleteMany();
+
+    console.log('Data Destroyed...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
