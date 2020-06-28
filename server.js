0d0612f784b11d159129ce3521b62a182f013b06
myates99 <zeac101@live.rhul.ac.uk>
2020-06-28 14:21:36 +0100

    Add security headers and XSS protection

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const colors = require('colors');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -33,6 +35,12 @@ if (process.env.NODE_ENV === 'development') {
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Mount routers
 app.use('/api/v1/articles', articles);
