d7d2e2ea54dc50c63755f56494b30a6f9d760aa1
myates99 <zeac101@live.rhul.ac.uk>
2020-06-28 14:29:01 +0100

    Limit no. requests and protect against HPP attacks

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -41,6 +43,17 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
 
 // Mount routers
 app.use('/api/v1/articles', articles);
